737fceffe83b9aca22e06a0803c425ad266c1485
Bartek Makowski <biuro@mackode.pl>
2024-11-04 20:24:02 +0000

    Add tap events

---
+++ tapped-rect.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+  "fyne.io/fyne/v2"
+  "fyne.io/fyne/v2/canvas"
+  "fyne.io/fyne/v2/widget"
+  col "golang.org/x/image/colornames"
+)
+
+type TapRect struct {
+  widget.BaseWidget
+  rect *canvas.Rectangle
+  cb func(fyne.Position)
+}
+
+func NewTapRect(width, height float32, cb func(fyne.Position)) *TapRect {
+  tc := &TapRect{}
+  tc.ExtendBaseWidget(tc)
+  tc.rect = drawRectangle(col.Grey, 0, 0, width, height)
+  tc.cb = tc.cb
+  return tc
+}
+
+func (t *TapRect) CreateRenderer() fyne.WidgetRenderer {
+  return widget.NewSampleRenderer(t.rect)
+}
+
+func (t *TapRect) Tapped(ev *fyne.PointEvent) {
+  t.cb(ev.Position)
+}
+
+func (t *TapRect) TappedSecondary(_ *fyne.PointEvent) {}
+
